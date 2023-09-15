d5ad68491e6290fbbf127d611767ab8a230e94f6
Adrian Ross <amr424@lehigh.edu>
2023-09-14 20:09:31 -0400

    Fix bug #1

--- acapy_resolver_github/__init__.py
+++ acapy_resolver_github/__init__.py
@@ -1,13 +1,13 @@
 """ACA-Py Resolver Github."""
 
 from aries_cloudagent.config.injection_context import InjectionContext
-from aries_cloudagent.resolver.did_resolver import DIDResolverRegistry
+from aries_cloudagent.resolver.did_resolver import DIDResolver
 
 from .resolver import OracleResolver
 
 
 async def setup(context: InjectionContext):
     """Setup the plugin."""
-    registry = context.inject(DIDResolverRegistry)
-    assert isinstance(registry, DIDResolverRegistry)
+    registry = context.inject(DIDResolver)
+    assert isinstance(registry, DIDResolver)
     registry.register(OracleResolver())
